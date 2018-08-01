6d0659dfb3879fb0335e843fcea528318e007447
Bing Leng <icyice80@gmail.com>
2018-08-01 04:30:14 -0700

    update model

--- CosmosDBDemo/Model/Block.cs
+++ CosmosDBDemo/Model/Block.cs
@@ -2,10 +2,17 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Newtonsoft.Json;
 
 namespace CosmosDBDemo.Model
 {
     public class Block
     {
+        [JsonProperty("id")]
+        public string Id { get; set; }
+
+        [JsonProperty("block_num")]
+        public string BlockNumber { get; set; }
+
     }
 }
--- CosmosDBDemo/Services/CosmosDBDocumentCollection.cs
+++ CosmosDBDemo/Services/CosmosDBDocumentCollection.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
 using CosmosDBDemo.Model;
 using Microsoft.Azure.Documents.Client;
